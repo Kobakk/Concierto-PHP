38ea09743b15a7134e4f628de6426aa7df8e84f1
kobakk <davidyyhj@gmail.com>
2024-04-23 11:07:33 +0200

    Rutas solucionadas

--- fuente/Controlador/entradaController.php
+++ fuente/Controlador/entradaController.php
@@ -4,15 +4,15 @@ class entradaController
 {
   public function inicio()
   {
-    require_once __DIR__ . '/../../fuente/Repositorio/TicketsRepositorio.php';
-    $todo = (new TicketsRepositorio())->getTickets();
+    require_once __DIR__ . '/../../fuente/Repositorio/ActuacionRepositorio.php';
+    $todo = (new ActuacionRepositorio())->getTodo();
     var_dump($todo);
     require_once __DIR__ . '/../../app/plantillas/pedirEntrada.php';
   }
   public function comprobarHoraGrupo()
   {
-    require_once __DIR__ . '/../../fuente/Repositorio/TicketsRepositorio.php';
-    $banda =  (new TicketsRepositorio())->getBanda($_POST['opciones']);
+    require_once __DIR__ . '/../../fuente/Repositorio/ActuacionRepositorio.php';
+    $banda =  (new ActuacionRepositorio())->getBanda($_POST['opciones']);
     var_dump($banda);
 
     if (sizeof($banda) > 1) {
