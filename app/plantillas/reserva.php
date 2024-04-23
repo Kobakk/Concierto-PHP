d4583d95c55eb5154bd264cc2c45680df754c821
kobakk <davidyyhj@gmail.com>
2024-04-23 11:35:37 +0200

    Validador DNI

--- app/plantillas/reserva.php
+++ app/plantillas/reserva.php
@@ -3,7 +3,7 @@
     <label for="dni">DNI :</label>
     <input type="text" name="dni">
     <label for="localidades">Localidades: </label>
-    <input type="number" name="localidades" min="0" max="5">
+    <input type="number" name="localidades" min="1" max="5">
     <br>
     <button type="submit">Enviar</button>
 </form>
--- fuente/Controlador/reservaController.php
+++ fuente/Controlador/reservaController.php
@@ -3,7 +3,19 @@ class reservaController{
     public function reserva(){
         var_dump($_SESSION);
        if(isset($_POST)) {
-        $solucion = $_POST;
+            if(empty($_POST['dni']) || empty ($_POST['localidades'])){
+                $_SESSION['error'] = "Especifique dni y localidades";
+            } else{
+                $dniPatron = "/^[0-9]{8}[a-zA-Z]$/";
+                if(!preg_match($dniPatron, $_POST['dni'])){
+                    $_SESSION['error'] = "Formato de DNI incorrecto";                    
+                } else{
+                    $_SESSION['usuario']['dni'] = $_POST['dni'];
+                    $_SESSION['usuario']['localidades'] = $_POST['localidades'];
+                    header('Location: index.php?ctl=inicio');
+                }
+            }
+
        }
     require_once __DIR__ . '../../../app/plantillas/reserva.php';
     }
