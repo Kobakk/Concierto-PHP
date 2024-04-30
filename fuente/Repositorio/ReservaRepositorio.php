9018e573ef555fbcf670bfd18a9c32f4da9848a5
kobakk <davidyyhj@gmail.com>
2024-04-30 21:55:49 +0200

    Funcion

--- fuente/Repositorio/ReservaRepositorio.php
+++ fuente/Repositorio/ReservaRepositorio.php
@@ -21,7 +21,27 @@ class ReservarRepositorio{
             if(isset($snt)) unset($snt);
             if(isset($con)) $con=null;
         }
-
+    }
+    
+    public function reservaCorrecta(string $dni, array $datosReserva):int {
+        $sql = 'INSERT INTO dbo.Reserva (dni, fecha, hora,idActuacion, localidades, pagado)
+        VALUES(:dni, :fecha, :hora,:idActuacion,:localidades, :pagado);'; 
+        try{
+            $con = (new ConexionBd())->getConexion();
+            $snt = $con->prepare($sql);
+            $snt->bindValue(':dni', $dni);
+            $snt->bindValue(':fecha', $datosReserva['fecha']);
+            $snt->bindValue(':hora', $datosReserva['hora']);
+            $snt->bindValue(':idActuacion', $datosReserva['id']);
+            $snt->bindValue(':localidades', $datosReserva['localidades']);             
+            $snt->execute();
+            return $con->lastInsertId();
+        } catch (\PDOException $ex){
+            throw $ex;
+        } finally {
+            if(isset($snt)) unset($snt);
+            if(isset($con)) $con=null;
+        }      
     }
 
     public function confirmarReserva(int $id){
